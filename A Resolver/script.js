52af01941d42d4d0bbf743a1b29160d8a45ddcf3
Zoe Acquistapace <zodoze8@gmail.com>
2026-03-25 08:43:25 -0300

    arreglos script

--- A Resolver/script.js
+++ A Resolver/script.js
@@ -1,12 +1,16 @@
-const input = document.getElementById("guessinput")
+const input = document.getElementById("guessInput")
 const button = document.getElementById("guessBtn")
 const message = document.getElementById("message")
 const attemptsText = document.getElementById("attempts")
+const reiniciar = document.getElementById("resetBtn")
 
-let secretNumber = Math.random() * 100
+let secretNumber = Math.floor(Math.random() * 100); 
+
 let attempts = 0
 
-button.addEventListener("click", checkGuess())
+button.onclick = checkGuess;
+
+reiniciar.onclick = reinicioJuego;
 
 function checkGuess(){
 
@@ -16,10 +20,11 @@ function checkGuess(){
 
   attemptsText.innerText = attempts
 
-  if(guess === secretNumber){
-    message.innerText = "¡Ganaste!"
+  if (guess == secretNumber)
+  {
+    message.innerText = "¡Ganaste!";
+    setTimeout(() => { reinicioJuego(); }, 1000);
   }
-
   if(guess < secretNumber){
     message.innerText = "El número es mayor"
   }
@@ -28,4 +33,14 @@ function checkGuess(){
     message.innerText = "El número es menor"
   }
 
+}
+
+function reinicioJuego () 
+{
+  
+  secretNumber = Math.floor(Math.random() * 100); 
+  attempts = 0; 
+  attemptsText.innerText = attempts;
+  message.innerText = "Nuevo juego, intenta adivinar";
+  input.value = "";
 }
